b66c98fca5f625526e2f51f1e80dd154729b9e54
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-10-06 13:58:24 +0300

    finish SignUpVeiw

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
-  static const String login="/login";
-  static const String home="/HomeView";
+  static const String login = "/login";
+  static const String signUp = "/Signup";
+  static const String home = "/HomeView";
 }
--- lib/core/common/ui/custom_button.dart
+++ lib/core/common/ui/custom_button.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:laza_app/core/theme/app_text_style.dart';
+import 'package:laza_app/core/theme/color_manager.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.title});
+  const CustomButton({super.key, required this.title, this.onTab});
 
   final String title;
 
+  final void Function()? onTab;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 52.h,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF9C7BFF),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.r),
-          ),
-        ),
-        onPressed: () {},
-        child: Text(title, style: TextStyle(fontSize: 16, color: Colors.white)),
+    return InkWell(
+      onTap: onTab,
+      child: Container(
+        width: double.infinity,
+        height: 75.h,
+        alignment: Alignment.center,
+        color: ColorManager.move,
+        child: Text(title, style: AppTextStyle.fontWhite17Inter),
       ),
     );
   }
